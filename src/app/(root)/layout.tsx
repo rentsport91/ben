23bf1dd93f436a723c9a9d7b028e12d4c22296db
rentsport91 <rentsport91@gmail.com>
2025-05-23 08:01:08 +0000

    update v11

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,7 +2,6 @@ import { Header } from "@/components/header";
 import { Footer } from "@/components/Footer";
 import { ReactNode } from "react";
 import ChatWidget from "@/components/features/chatbot/chat-widget";
-import "public/google5c1470dafb6e2f0a.html";
 
 const PublicLayout = async ({ children }: { children: ReactNode }) => {
   return (
---
+++ src/app/sitemap.ts
@@ -0,0 +1,22 @@
+export default function Sitemap() {
+  return [
+    {
+      url: "https://www.amermaxlogistics.org/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://www.amermaxlogistics.org/support",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://www.amermaxlogistics.org/track",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.4,
+    },
+  ];
+}
